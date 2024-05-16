284e2135188327fe634ff0fbd1b6cc0a283c8e07
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-16 19:14:05 +0300

    style: Improve grid layout and styling for todo items

--- helpers/renderProjectDom.js
+++ helpers/renderProjectDom.js
@@ -6,67 +6,68 @@ import trash from "../imgs/trash.png";
 
 // Function to render projects to the DOM
 export function renderProjectsDom() {
-  // Reference to the projects array
-  let projects = getProjects();
+    // Reference to the projects array
+    let projects = getProjects();
 
-  // Call the firstProjectToActive function and pass the projects array to it
-  firstProjectToActive(projects);
+    // Call the firstProjectToActive function and pass the projects array to it
+    firstProjectToActive(projects);
 
-  // Get the projects div from the DOM and clear it to render the updated projects array
-  const projectsUi = document.querySelector(".projects");
-  projectsUi.innerHTML = "";
+    // Get the projects div from the DOM and clear it to render the updated projects array
+    const projectsUi = document.querySelector(".projects");
+    projectsUi.innerHTML = "";
 
-  // Loop through the projects array and create a div for each project
-  projects.forEach((project) => {
-    // Create a container for the project
-    const projectContainer = document.createElement("div");
-    projectContainer.setAttribute("data-id", project.id);
+    // Loop through the projects array and create a div for each project
+    projects.forEach((project) => {
+        // Create a container for the project
+        const projectContainer = document.createElement("div");
+        projectContainer.setAttribute("data-id", project.id);
 
-    // Create an element for the project name
-    const projectName = document.createElement("h2");
-    projectName.classList.add("project-name");
+        // Create an element for the project name
+        const projectName = document.createElement("h2");
+        projectName.classList.add("project-name");
 
-    // Create an element for the delete button
-    const projectDelete = document.createElement("img");
-    projectDelete.classList.add("delete-project");
-    projectDelete.src = trash;
+        // Create an element for the delete button
+        const projectDelete = document.createElement("img");
+        projectDelete.classList.add("delete-project");
+        projectDelete.setAttribute("alt", "delete-project");
+        projectDelete.src = trash;
 
-    // Call the makeProjectActive function and pass the project to it
-    makeProjectActive(project, projectContainer);
+        // Call the makeProjectActive function and pass the project to it
+        makeProjectActive(project, projectContainer);
 
-    // Add an event listener to the delete button to call the deleteProject function
-    projectDelete.addEventListener("click", () => {
-      // Call the deleteProject function and pass the project id to it
-      deleteProject(project.id);
+        // Add an event listener to the delete button to call the deleteProject function
+        projectDelete.addEventListener("click", () => {
+            // Call the deleteProject function and pass the project id to it
+            deleteProject(project.id);
+        });
+
+        // Set the project name text content
+        projectName.textContent = project.name;
+
+        // Append elements to the project container
+        projectContainer.appendChild(projectName);
+        projectContainer.appendChild(projectDelete);
+
+        // Append the project container to the projects UI
+        projectsUi.appendChild(projectContainer);
     });
 
-    // Set the project name text content
-    projectName.textContent = project.name;
-
-    // Append elements to the project container
-    projectContainer.appendChild(projectName);
-    projectContainer.appendChild(projectDelete);
-
-    // Append the project container to the projects UI
-    projectsUi.appendChild(projectContainer);
-  });
-
-  // Call the renderTodo function to update the todos in the DOM
-  renderTodo();
+    // Call the renderTodo function to update the todos in the DOM
+    renderTodo();
 }
 
 // Function to make the project active or inactive based on the active property
 function makeProjectActive(project, projectContainer) {
-  projectContainer.dataset.active = project.active;
+    projectContainer.dataset.active = project.active;
 }
 
 // Function to select the first project of the projects array to make it active
 // If all projects have data-active="false," then the first project will be selected
 function firstProjectToActive(projects) {
-  // If all projects have data-active="false," then the first project will be selected.
-  // The every method will return true if all projects have data-active="false"
-  if (projects.every((project) => project.active === false)) {
-    // Set the first project to active
-    projects[0] ? (projects[0].active = true) : null;
-  }
+    // If all projects have data-active="false," then the first project will be selected.
+    // The every method will return true if all projects have data-active="false"
+    if (projects.every((project) => project.active === false)) {
+        // Set the first project to active
+        projects[0] ? (projects[0].active = true) : null;
+    }
 }
--- helpers/renderTodoDom.js
+++ helpers/renderTodoDom.js
@@ -10,106 +10,108 @@ import { nameInput, detailsInput } from "../scripts/validateTodo";
 
 // Function to render the todo list to the DOM based on the active project
 export function renderTodo() {
-  // Get the projects array from local storage
-  const projects = getProjects();
+    // Get the projects array from local storage
+    const projects = getProjects();
 
-  // Find the active project from the projects array
-  const activeProject = projects.find((project) => project.active === true);
+    // Find the active project from the projects array
+    const activeProject = projects.find((project) => project.active === true);
 
-  // Get the todos array from the active project
-  const todos = activeProject?.todos;
+    // Get the todos array from the active project
+    const todos = activeProject?.todos;
 
-  // Get the todo list element from the DOM and clear it
-  const todoList = document.querySelector("main");
-  todoList.innerHTML = "";
+    // Get the todo list element from the DOM and clear it
+    const todoList = document.querySelector("main");
+    todoList.innerHTML = "";
 
-  // Loop through the todos array and create a div for each todo
-  todos?.forEach((todo) => {
-    // Create the section for the todo
-    const todoContainer = document.createElement("section");
-    todoContainer.classList.add("todo");
+    // Loop through the todos array and create a div for each todo
+    todos?.forEach((todo) => {
+        // Create the section for the todo
+        const todoContainer = document.createElement("section");
+        todoContainer.classList.add("todo");
 
-    // Create the main div for the todo
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo-div");
+        // Create the main div for the todo
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo-div");
 
-    // Create the div for the todo name
-    const todoName = document.createElement("h2");
-    todoName.classList.add("todo-name");
-    todoName.textContent = todo.name;
+        // Create the div for the todo name
+        const todoName = document.createElement("h2");
+        todoName.classList.add("todo-name");
+        todoName.textContent = todo.name;
 
-    // Create the div for the todo delete button
-    const todoDelete = document.createElement("img");
-    todoDelete.classList.add("delete-todo");
-    todoDelete.src = deleteImg;
+        // Create the div for the todo delete button
+        const todoDelete = document.createElement("img");
+        todoDelete.classList.add("delete-todo");
+        todoDelete.setAttribute("alt", "delete-todo");
+        todoDelete.src = deleteImg;
 
-    // Add an event listener to the delete button
-    todoDelete.addEventListener("click", () => {
-      // Call the deleteTodo function and pass the todo id to it
-      deleteTodo(todo.id, activeProject.id);
+        // Add an event listener to the delete button
+        todoDelete.addEventListener("click", () => {
+            // Call the deleteTodo function and pass the todo id to it
+            deleteTodo(todo.id, activeProject.id);
+        });
+
+        // Create the todo edit button
+        const todoEdit = document.createElement("button");
+        todoEdit.setAttribute("id", "edit");
+        todoEdit.textContent = "Edit";
+
+        // Add an event listener to the edit button
+        todoEdit.addEventListener("click", () => {
+            // Call the editTodo function and pass the todo id to it
+            showTodoForm();
+            showCover(50);
+            document.querySelector(".todo-form").classList.add("edit");
+            setEditIds({
+                todoId: todo.id,
+                projectId: activeProject.id,
+            });
+            setFormValues(todo);
+
+            // Validate input using the nameInput and detailsInput functions
+            const name = document.querySelector("#todo-name-input");
+            const details = document.querySelector("#todo-details-input");
+            nameInput(name);
+            detailsInput(details);
+        });
+
+        // Create the div for deadline
+        const todoDeadline = document.createElement("p");
+        todoDeadline.classList.add("todo-deadline");
+        todoDeadline.textContent = todo.deadline;
+
+        // Create the edit icon
+        const editImg = document.createElement("img");
+        editImg.setAttribute("alt", "edit-todo");
+        editImg.src = editSrc;
+
+        // Create the div for details
+        const todoDetails = document.createElement("p");
+        todoDetails.classList.add("todo-details");
+        todoDetails.textContent = todo.details;
+
+        // Create the span for priority color
+        const priorityColor = document.createElement("span");
+        const priorityClass = todo.priority.toLowerCase().replace(" ", "-");
+        priorityColor.classList.add("todo-priority");
+        priorityColor.classList.add(priorityClass);
+
+        // Add the edit icon to the edit button
+        todoEdit.appendChild(editImg);
+
+        // Create the container for todo buttons
+        const todoButtonsContainer = document.createElement("div");
+        todoButtonsContainer.classList.add("todo-buttons-container");
+        todoButtonsContainer.appendChild(todoDeadline);
+        todoButtonsContainer.appendChild(todoEdit);
+
+        // Append everything to the todo container
+        todoDiv.appendChild(todoDelete);
+        todoDiv.appendChild(priorityColor);
+        todoDiv.appendChild(todoName);
+        todoDiv.appendChild(todoDetails);
+        todoDiv.appendChild(todoButtonsContainer);
+
+        todoContainer.appendChild(todoDiv);
+        todoList.appendChild(todoContainer);
     });
-
-    // Create the todo edit button
-    const todoEdit = document.createElement("button");
-    todoEdit.setAttribute("id", "edit");
-    todoEdit.textContent = "Edit";
-
-    // Add an event listener to the edit button
-    todoEdit.addEventListener("click", () => {
-      // Call the editTodo function and pass the todo id to it
-      showTodoForm();
-      showCover(50);
-      document.querySelector(".todo-form").classList.add("edit");
-      setEditIds({
-        todoId: todo.id,
-        projectId: activeProject.id,
-      });
-      setFormValues(todo);
-
-      // Validate input using the nameInput and detailsInput functions
-      const name = document.querySelector("#todo-name-input");
-      const details = document.querySelector("#todo-details-input");
-      nameInput(name);
-      detailsInput(details);
-    });
-
-    // Create the div for deadline
-    const todoDeadline = document.createElement("p");
-    todoDeadline.classList.add("todo-deadline");
-    todoDeadline.textContent = todo.deadline;
-
-    // Create the edit icon
-    const editImg = document.createElement("img");
-    editImg.src = editSrc;
-
-    // Create the div for details
-    const todoDetails = document.createElement("p");
-    todoDetails.classList.add("todo-details");
-    todoDetails.textContent = todo.details;
-
-    // Create the span for priority color
-    const priorityColor = document.createElement("span");
-    const priorityClass = todo.priority.toLowerCase().replace(" ", "-");
-    priorityColor.classList.add("todo-priority");
-    priorityColor.classList.add(priorityClass);
-
-    // Add the edit icon to the edit button
-    todoEdit.appendChild(editImg);
-
-    // Create the container for todo buttons
-    const todoButtonsContainer = document.createElement("div");
-    todoButtonsContainer.classList.add("todo-buttons-container");
-    todoButtonsContainer.appendChild(todoDeadline);
-    todoButtonsContainer.appendChild(todoEdit);
-
-    // Append everything to the todo container
-    todoDiv.appendChild(todoDelete);
-    todoDiv.appendChild(priorityColor);
-    todoDiv.appendChild(todoName);
-    todoDiv.appendChild(todoDetails);
-    todoDiv.appendChild(todoButtonsContainer);
-
-    todoContainer.appendChild(todoDiv);
-    todoList.appendChild(todoContainer);
-  });
 }
